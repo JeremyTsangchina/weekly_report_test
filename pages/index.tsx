db2a786a5fb964ec6e07c195fb11b1f6339d154f
JeremyTsangchina <125327070+JeremyTsangchina@users.noreply.github.com>
2023-02-14 17:45:43 +0800

    优化调整

--- pages/index.tsx
+++ pages/index.tsx
@@ -99,7 +99,7 @@ const Home: NextPage = () => {
 
       <a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/guaguaguaxia/weekly_report"
+          href="https://github.com/JeremyTsangchina/weeklytest"
           target="_blank"
           rel="noopener noreferrer"
         >
