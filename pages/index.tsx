d471dc0b4165718d989563b7f9c658002b1ab6a0
JeremyTsangchina <125327070+JeremyTsangchina@users.noreply.github.com>
2023-02-17 00:20:04 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -86,8 +86,8 @@ const Home: NextPage = () => {
   };
 
   return ( 
-    <div className="flex max-w-5xl mx-auto flex-col justify-center items-center justify-center py-2 min-h-screen">
-      <Head>
+    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Head className="justify-center">
         <title>{t('title')}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
