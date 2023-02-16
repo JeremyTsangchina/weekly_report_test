b17b8390db808e320bfc8f8329252cf8a1b27873
JeremyTsangchina <125327070+JeremyTsangchina@users.noreply.github.com>
2023-02-16 23:46:42 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -229,7 +229,7 @@ const Home: NextPage = () => {
                     {t('step3')}{" "}
                 </p>
             </div>
-              <div className="flex mt-10 items-center space-x-3">
+              <div className="flex mt-10 items-center justify-center text-center space-x-3">
                 <Image
                   src="/QR.png"
                   width={100}
